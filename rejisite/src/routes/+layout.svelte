130b51902ed78643591f1319680947b1dc256aa0
MS <madhsa1973@gmail.com>
2026-02-18 12:07:28 -0500

    floating teeth

--- rejisite/src/routes/+layout.svelte
+++ rejisite/src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
-	import "../app.css";
+  let { children } = $props();
+  import "../app.css";
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children()}
